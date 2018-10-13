0d30b3fd2c3c5b718670166f6f2f1d8e888dbe6d
Jeongyeon Christine Kim <christinejyeon@gmail.com>
2018-10-13 14:46:23 -0400

    Update tgm-descriptor-cleaning.R
    Tried to encode the characters with french accents but failed; used iconv, stri_trans_general, chartr but all didn't work. stri_trans_general and chartr didn't make any changes to the file but at least iconv made the strings NA so I removed them all. Only 24 rows were removed.

--- tgm-descriptor-cleaning.R
+++ tgm-descriptor-cleaning.R
@@ -2,9 +2,11 @@ setwd("/Users/Christine/Documents/everyday-practice-r")
 
 tgmfile <- read.delim("tgm1.xml")
 # tgmfile1 <- read.delim("tgm1.txt")
+temp <- read.delim("tgm1.xml")
 
 names(tgmfile)[1] <- "Description"
 tgmfile$Description <- as.character(tgmfile$Description)
+
 tgmfile$Description <- gsub("^ ", "", tgmfile$Description)
 tgmfile <- subset(tgmfile, (substr(Description,1,4)=="<NON")|(substr(Description,1,4)=="<DES"))
 tgmfile$Description <- gsub("^<NON-DESCRIPTOR>","", tgmfile$Description)
@@ -13,3 +15,13 @@ tgmfile$Description <- gsub("^<DESCRIPTOR>","", tgmfile$Description)
 # tgmfile$Description <- ifelse(substr(tgmfile$Description,1,4)=="<NON",gsub("<NON-DESCRIPTOR>","", tgmfile$Description), gsub("<DESCRIPTOR>","",tgmfile$Description))
 tgmfile$Description <- gsub("</NON-DESCRIPTOR>$","",tgmfile$Description)
 tgmfile$Description <- gsub("</DESCRIPTOR>$","",tgmfile$Description)
+
+row.names(tgmfile) <- NULL
+tgmfile$Description <- iconv(tgmfile$Description,"latin1","ASCII")
+tgmfile <- subset(tgmfile, !is.na(Description))
+
+##### Encoding french accents into utf-8
+
+save(tgmfile, file="tgmfile.Rda")
+library(openxlsx)
+write.xlsx(tgmfile, file="tgmdescriptors.xlsx")
